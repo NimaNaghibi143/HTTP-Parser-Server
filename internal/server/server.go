9f14fe2972a2142d663f124417a5962debaf402d
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-03-10 22:12:07 +0330

    feat: implement server handler and runner in the server package with go routines

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,36 @@
 package server
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+}
+
+func runServer(s *Server, listener net.Listener) error {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return
+		}
+	}
+
+}
+
+func Serve(port uint16) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%d", port))
+
+	if err != nil {
+		return nil, err
+	}
+
+	server := &Server{}
+	go runServer(server, listener)
+
+	return server, nil
+}
+
+func (s *Server) Close() error {
+	return nil
+}
