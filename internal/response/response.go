9833c6cb970a25a7fee769a22ffc81f3cbacd025
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-03-11 11:00:07 +0330

    feat: create enums for the response status codes

--- internal/response/response.go
+++ internal/response/response.go
@@ -1,1 +1,12 @@
 package response
+
+type Response struct {
+}
+
+type StatusCode int
+
+const (
+	StatusOk                  StatusCode = 200
+	StatusBadRequest          StatusCode = 400
+	StatusInternalServerError StatusCode = 500
+)
