1ba0d34740fa01f82d17a31184e284816bc53b0a
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-03-13 17:59:57 +0330

    feat: implement a func to write headres on at a time

--- internal/response/response.go
+++ internal/response/response.go
@@ -3,6 +3,8 @@ package response
 import (
 	"fmt"
 	"io"
+
+	"http.nima.strive/internal/headers"
 )
 
 type Response struct {
@@ -15,6 +17,29 @@ const (
 	StatusBadRequest          StatusCode = 400
 	StatusInternalServerError StatusCode = 500
 )
+
+func GetDefaultHeaders(contentLen int) *headers.Headers {
+	h := headers.NewHeaders()
+
+	h.Set("Content-Length", fmt.Sprintf("%d", contentLen))
+	h.Set("Connection", "close")
+	h.Set("Content-Type", "text/plain")
+
+	return h
+}
+
+func WriteHeader(w io.Writer, h *headers.Headers) error {
+	b := []byte{}
+
+	h.ForEach(func(n, v string) {
+		b = fmt.Appendf(b, "%s: %s\r\n", n, v)
+	})
+
+	b = fmt.Append(b, "\r\n")
+	_, err := w.Write(b)
+
+	return err
+}
 
 func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
 	statusLine := []byte{}
